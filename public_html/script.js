3688c6240c18fa988b3acffb684d0eeca3698811
aaron cachola <miakelcachola@hotmail.com>
2015-01-21 14:52:45 -0800

    first commit

---
+++ public_html/script.js
@@ -0,0 +1,70 @@
+$("document").ready(function(){
+
+$(".yeah").css("background-color", "yellow");
+
+$(".projectColor").css("background-color", "purple");
+
+$(".k").css("color", "orange");
+
+
+
+
+
+
+$('#slideAway').bind('click', slideTheImage);
+
+$('#showLogo').bind('click', showTheImage);
+
+$('#hideLogo').bind('click', hideTheImage);
+
+$('#showLogov2').bind('click', showTheImagev2);
+
+$('#hideLogov2').bind('click', hideTheImagev2);
+
+
+
+
+$('#superHumans').accordion({header: "h3"});
+
+});
+
+function slideTheImage() {
+$('#logo').slideUp(2500);
+}
+
+function showTheImage() {
+$('#logo').show('fold', {},2500);
+}
+
+function hideTheImage() {
+$('#logo').hide('fold', {},2500);
+}
+
+function showTheImagev2() {
+$('#logo').show('explode', {},2500);
+}
+
+function hideTheImagev2() {
+$('#logo').hide('explode', {},2500);
+}
+
+$("document").ready(function(){
+$(".red").css("background-color", "red");
+});
+
+$("document").ready(function(){
+$(".yeah").css("color", "white");
+});
+
+
+$("document").ready(function(){
+
+$("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+
+$('h1').bind('click', mouseClick);
+});
+
+function mouseOverMe() {
+$("h1").html("or Portfolio 3");
+}
+
